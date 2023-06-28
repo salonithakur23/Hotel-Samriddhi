54c95803b7e0a21adcb83a0953b526ea14177193
salonithakur23 <119155996+salonithakur23@users.noreply.github.com>
2023-06-28 17:49:15 +0530

    Item list crud

--- frontend/src/components/Pages/MainAdmin/Item/EditItem.js
+++ frontend/src/components/Pages/MainAdmin/Item/EditItem.js
@@ -1,46 +1,53 @@
-import React, { useState } from 'react'
-// import HotelSidebar from '../../HotelSidebar'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 import { Container, Col, Row, Table, Button } from 'react-bootstrap'
 import { AiFillDashboard, AiFillDelete, AiFillEdit, AiFillSetting } from 'react-icons/ai';
 import { RiArrowGoBackLine } from 'react-icons/ri';
 import Form from 'react-bootstrap/Form';
 import { IoIosCreate } from 'react-icons/io';
 import { Link } from 'react-router-dom';
-// import './RoomBooking.css'
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
-const Item = () => {
-    const navigate = useNavigate()
-    const [item_Name, setItem_Name] = useState(null);
-    const [price, setPrice] = useState(null);
-    const [category_Name, setCategory_Name] = useState(null);
 
+const EditItem = () => {
+    const params = useParams();
+    const navigate = useNavigate();
+    const [getSpecificItem, setSpecificItem] = useState("");
+    const [Item_Name, setItem_Name] = useState(getSpecificItem.Item_Name);
+    const [price, setPrice] = useState(getSpecificItem.price);
+    const [Category_Name, setCategory_Name] = useState(getSpecificItem.Category_Name);
+
+    console.log(getSpecificItem, "Check id from url")
+
+    useEffect(() => {
+        axios.get(`http://localhost:4000/api/v1/item/${params.id}`).then((response) => {
+            setSpecificItem(response.data);
+        })
+    }, [])
 
 
     const submitform = () => {
         try {
-            axios.post("http://localhost:4000/api/v1/item/new", {
-                "Item_Name": item_Name,
-                "price": price,
-                "Category_Name": category_Name,
+            axios.put(`http://localhost:4000/api/v1/item/${params.id})`, {
+                "Item_Name":Item_Name,
+                "price":price,
+                "Category_Name":Category_Name,
             })
-            alert("Data submitted successfully")
+            alert("Item update Successfully")
             navigate("/item-list")
         } catch (error) {
             console.log(error.response)
 
         }
     }
+
     return (
         <>
-
-
             <Container style={{ width: "90%", marginTop: "20px" }} >
                 <Table striped bordered hover className='main-table'>
                     <thead>
                         <tr>
-                            <th><h5><AiFillDashboard /> &nbsp;Dasboard / Add New Item</h5></th>
+                            <th><h5><AiFillDashboard /> &nbsp;Dasboard / Edit Item</h5></th>
                         </tr>
                     </thead>
                 </Table>
@@ -72,27 +79,23 @@ const Item = () => {
 
                             <div class="col-md-4 position-relative">
                                 <label className="label">Item Name</label>
-                                <input type="text" class="form-control" value={item_Name} onChange={(e) => setItem_Name(e.target.value)} required
+                                <input type="text" className="form-control" onChange={(e) => setItem_Name(e.target.value)} id="inputname" placeholder={getSpecificItem.Item_Name} required />
 
-                                />
 
                             </div>
 
                             <div class="col-md-4 position-relative">
                                 <label className="label">Price.</label>
-                                <input type="text" class="form-control" value={price} onChange={(e) => setPrice(e.target.value)} required
+                                <input type="text" className="form-control" onChange={(e) => setPrice(e.target.value)} id="inputname" placeholder={getSpecificItem.price} required />
 
-                                />
 
                             </div>
 
 
                             <div class="col-md-4 position-relative">
                                 <label className="label">Category Name</label>
-                                <input type="text" class="form-control" value={category_Name} onChange={(e) => setCategory_Name(e.target.value)} required
+                                <input type="text" className="form-control" onChange={(e) => setCategory_Name(e.target.value)} id="inputname" placeholder={getSpecificItem.Category_Name} required />
 
-
-                                />
 
                             </div>
                             <center>
@@ -111,12 +114,8 @@ const Item = () => {
                     </Row>
                 </Container>
             </div>
-
-
-
-
         </>
     )
 }
 
-export default Item
+export default EditItem
