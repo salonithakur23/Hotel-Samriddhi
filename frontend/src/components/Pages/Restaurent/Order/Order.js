c84d489b4316f4fe0d4a15c5c0d096e2dba6309a
Bhuvnesh Saini <97789468+bhuvnesh63@users.noreply.github.com>
2023-07-03 17:51:33 +0530

    Post the data of order

--- frontend/src/components/Pages/Restaurent/Order/Order.js
+++ frontend/src/components/Pages/Restaurent/Order/Order.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import './Order.css'
-import { Button, Col, Container, Row, Table } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import './Order.css';
+import { Button, Col, Container, Row, Table } from 'react-bootstrap';
 import { AiFillDashboard } from 'react-icons/ai';
 import Form from 'react-bootstrap/Form';
 import { IoIosCreate } from 'react-icons/io';
@@ -8,110 +8,103 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 
-const baseURL = "http://localhost:4000/api/v1/categories"
-
-const allItem = "http://localhost:4000/api/v1/items?Category_Name="
+const baseURL = "http://localhost:4000/api/v1/categories";
+const allItem = "http://localhost:4000/api/v1/items?Category_Name=";
 
 const Order = () => {
-
   const [get, setGetAll] = useState(null);
-
+  const [table_Number, setTable_Number] = useState('');
+  const [order_Time, setOrder_Time] = useState('');
+  const [category_Type, setCategory_Type] = useState('');
+  const [items, setItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(baseURL).then((response) => {
       setGetAll(response.data);
-      // console.log(response)
-    })
-  }, [])
-
-
-
-
-
-  const navigate = useNavigate()
-  const [table_Number, setTable_Number] = useState(null);
-  const [order_Time, setOrder_Time] = useState(null);
-  const [category_Type, setCategory_Type] = useState(null)
-  const [items, setItems] = useState([])
-  
-
-
-
-
-  const submitform = () => {
-    try {
-      axios.post("http://localhost:4000/api/v1/order/new", {
-        "Table_Number": table_Number,
-        "Order_Time": order_Time,
-        // "Category_Name": category_Type,
-      
-
-      })
-      alert("Order Submit Successfully")
-      navigate("/res-billing")
-    } catch (error) {
-      console.log(error.response)
-
-    }
-  }
+    });
+  }, []);
 
   const handleCategoriesItem = (val) => {
-    setCategory_Type(val)
+    setCategory_Type(val);
     axios.get(allItem + val).then((response) => {
       setItems(response.data.items);
-      // console.log(response,"vv")
-    })
+    });
+  };
 
+  const handleCheckboxChange = (event, item) => {
+    if (event.target.checked) {
+      setSelectedItems((prevSelectedItems) => [...prevSelectedItems, item]);
+    } else {
+      setSelectedItems((prevSelectedItems) =>
+        prevSelectedItems.filter((selectedItem) => selectedItem !== item)
+      );
+    }
+  };
 
-  }
+  const selectedItemsList = items?.map((item) => (
+    <div key={item.Item_Name}>
+      <Container>
+        <Row>
+          <Col sm={3}>
+            <Card style={{ width: "15rem" }}>
+              <Card.Body>
+                <Card.Text>
+                  <Form.Check
+                    aria-label="option 1"
+                    onChange={(e) => handleCheckboxChange(e, item)}
+                  />&nbsp;&nbsp;
+                  <p className="label">
+                    <span><b>Item Name</b></span>- {item.Item_Name}
+                  </p>
+                  <p><span><b>Price</b></span> -{item.price}</p>
+                  <div className="Opretor">
+                    <button className='decrease'>-</button>
+                    <p className='qnatity'>0</p>
+                    <button className='increase'>+</button>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  ));
 
+  const submitform = async () => {
+    if (!table_Number || !order_Time || selectedItems.length === 0) {
+      alert('Please fill in all the required fields and select at least one item.');
+      return;
+    }
 
+    try {
+      await axios.post("http://localhost:4000/api/v1/order/new", {
+        Table_Number: table_Number,
+        Order_Time: order_Time,
+        Items: selectedItems.map(item => ({
+          Item_Name: item.Item_Name,
+          Price: item.price,
+          Quantity: 1 // You can modify this value as per your requirements
+        }))
+      });
+      alert("Order Submitted Successfully");
+      navigate("/res-billing");
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
-  console.log(items)
-  const selectedItems = () => {
-    return items?.map((item) => {
-
-      return <div >
-        <Container >
-          <Row >
-            <Col sm={3}>
-              <Card style={{ width: "15rem" }}>
-                <Card.Body>
-                  <Card.Text>
-                    <Form.Check aria-label="option 1" />&nbsp;&nbsp;
-                    <p
-                    
-                      className="label"><span><b>Item Name</b></span>- {item.Item_Name} </p>
-                    <p
-                   
-                    ><span><b>Price</b></span> -{item.price}</p>
-                    <div className="Opretor">
-                      <button className='decrease'>-</button>
-                      <p className='qnatity'
-                      >0</p>
-                      <button  className='increase'>+</button>
-                    </div>
-
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-
-      </div>
-
-
-    })
-  }
   return (
     <>
-
-      <Container style={{ width: "90%", marginTop: "20px" }} >
+      <Container style={{ width: "90%", marginTop: "20px" }}>
         <Table striped bordered hover className='main-table'>
           <thead>
             <tr>
-              <th><h5><AiFillDashboard /> &nbsp;Dasboard / Add New Order</h5></th>
+              <th>
+                <h5><AiFillDashboard /> &nbsp;Dashboard / Add New Order</h5>
+              </th>
             </tr>
           </thead>
         </Table>
@@ -121,8 +114,7 @@ const Order = () => {
               <tr>
                 <th>
                   <div className='table-div'>
-
-                    <Button className='table-btn' variant="light" >
+                    <Button className='table-btn' variant="light">
                       <IoIosCreate />&nbsp;<Link to="/res-billing">Go Back</Link>
                     </Button>
                   </div>
@@ -134,73 +126,66 @@ const Order = () => {
         </Row>
       </Container>
 
-
-
-      <div className='form-div' >
+      <div className='form-div'>
         <Container>
           <Row>
-
-
-            <form className="row g-4 p-3 registration-form" >
-
-              <div class="col-md-4 position-relative">
+            <form className="row g-4 p-3 registration-form">
+              <div className="col-md-4 position-relative">
                 <label className="label">Table.no</label>
-                <input type="text" class="form-control"
-                  value={table_Number} onChange={(e) =>
-                    setTable_Number(e.target.value)} required
-
+                <input
+                  type="text"
+                  className="form-control"
+                  value={table_Number}
+                  onChange={(e) => setTable_Number(e.target.value)}
+                  required
                 />
-
               </div>
 
               <div className="col-md-4 position-relative">
                 <label className="label">Order Date & Time</label>
-                <input type="datetime-local" name="Booking_Date_Time" className="form-control"
-                  value={order_Time} onChange={(e) =>
-                    setOrder_Time(e.target.value)} required
-                // onChange={handleInputChange} value={Booking_Date_Time}
+                <input
+                  type="datetime-local"
+                  name="Booking_Date_Time"
+                  className="form-control"
+                  value={order_Time}
+                  onChange={(e) => setOrder_Time(e.target.value)}
+                  required
                 />
               </div>
 
-
-
-              <div className="col-md-4 position-relative"
-              >
+              <div className="col-md-4 position-relative">
                 <label className="form-label"> Category </label>
-                <Form.Select name="Room_Type"
+                <Form.Select
+                  name="Room_Type"
                   onChange={(e) => handleCategoriesItem(e.target.value)}
                 >
-                  <option value={category_Type}>select a category</option>
-                  {get?.categories?.map((items) => (
-                    <option >{items.Category_Type}</option>
+                  <option value={category_Type}>Select a category</option>
+                  {get?.categories?.map((category) => (
+                    <option key={category.Category_Type}>{category.Category_Type}</option>
                   ))}
                 </Form.Select>
               </div>
 
-              <Col sm={3} className='Item-container' >
-                {selectedItems()}
+              <Col sm={3} className='Item-container'>
+                {selectedItemsList}
               </Col>
 
               <center>
-                <Button className="stu_btn"
+                <Button
+                  className="stu_btn"
                   variant="success"
                   type="submit"
                   onClick={submitform}
                 >
                   Submit
                 </Button>
-
               </center>
-
             </form>
           </Row>
         </Container>
-      </div >
-
-
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
