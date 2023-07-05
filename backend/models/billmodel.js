eaeeeb39ef90898e9e92352e7dd636f56459aa97
Bhuvnesh Saini <97789468+bhuvnesh63@users.noreply.github.com>
2023-07-05 17:40:17 +0530

    KOT and BILL Printing

---
+++ backend/models/billmodel.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const billSchema = new mongoose.Schema({
+  orderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',
+    required: true,
+  },
+  resName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  gstNumber: {
+    type: String,
+    required: true,
+  },
+  bookingDateTime: {
+    type: Date,
+    required: true,
+  },
+  tableNumber: {
+    type: String,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  items: [
+    {
+      item: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price_after_Quantity: {
+        type: Number,
+        required: true,
+      }
+    },
+  ],
+});
+
+const Bill = mongoose.model('Bill', billSchema);
+module.exports = Bill;
+
